d6a54577acdb5cfe26bd249ef6f46e19a28f83da
Kavya.T <kavya.t@thoughtworks.com>
2026-05-18 00:06:33 +0530

    intentional failure

--- src/main/java/com/tw/github_action_demo/NPETestService.java
+++ src/main/java/com/tw/github_action_demo/NPETestService.java
@@ -3,11 +3,7 @@ package com.tw.github_action_demo;
 public class NPETestService {
 
     public void testNPE() {
-        Integer p = 11;
+        Integer p = null;
         System.out.println(p.compareTo(10));
     }
-}
-
-public String processData(String input) {
-    return input.toUpperCase();  // input could be null!
 }
