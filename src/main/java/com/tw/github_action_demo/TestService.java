2915b01c2ae2bc2e01a244c67d35b8092d7de023
arghyamitra <arghya.mitra@thoughtworks.com>
2026-05-18 14:52:54 +0530

    chcking for more complex issue

--- src/main/java/com/tw/github_action_demo/TestService.java
+++ src/main/java/com/tw/github_action_demo/TestService.java
@@ -5,10 +5,7 @@ import java.util.Objects;
 
 @Service
 public class TestService {
-    private final NPETestServiceImpl npeTestService;
-    public TestService(final NPETestServiceImpl npeTestService) {
-        this.npeTestService = Objects.requireNonNull(npeTestService, "npeTestService must not be null");
-    }
+    private NPETestServiceImpl npeTestService;
     void test() {
         npeTestService.testNPE();
     }
