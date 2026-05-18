267b6d64ad9bdb63d127bcaea5864664113b4d58
arghyamitra <arghya.mitra@thoughtworks.com>
2026-05-18 13:24:10 +0530

    checking for spring issues 2

--- src/test/java/com/tw/github_action_demo/GithubActionDemoApplicationTests.java
+++ src/test/java/com/tw/github_action_demo/GithubActionDemoApplicationTests.java
@@ -1,17 +1,18 @@
 package com.tw.github_action_demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GithubActionDemoApplicationTests {
 
+	@Autowired
+	TestService testService;
+
 	@Test
 	void contextLoads() {
-
-		NPETestServiceImpl npeTestService = new NPETestServiceImpl();
-
-		npeTestService.testNPE();
+		testService.test();
 	}
 
 }
