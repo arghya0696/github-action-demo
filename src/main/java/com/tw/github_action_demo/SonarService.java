2ec3f6c91cc5b91c7f32b841882ace3e4f4cd020
Sreelakshmi <sreelakshmi.adiga@thoughtworks.com>
2026-05-20 14:28:50 +0530

    bug

--- src/main/java/com/tw/github_action_demo/SonarService.java
+++ src/main/java/com/tw/github_action_demo/SonarService.java
@@ -1,12 +1,33 @@
 package com.tw.github_action_demo;
 
+import java.util.*;
+
 public class SonarService {
+    public String address = "London";
 
     public static final String name = "12321";
 
-    public String address = "London";
-
-    public void printer(){
-        System.out.println("Hellooo the name is "+name);
+    public void printer() {
+        System.out.println("Hellooo the name is " + name);
     }
+
+    public int getLength(List<String> items) {
+        String first = items.get(0);
+        return first.length();
+    }
+
+    public void riskyParse(String val) {
+        try {
+            int x = Integer.parseInt(val);
+            System.out.println(x);
+        } catch (NumberFormatException e) {
+        }
+    }
+
+    public String buildGreeting() {
+        String unused = "this is never used";
+        return "Hello " + name;
+    }
+
+    private String password = "superSecret123";
 }
