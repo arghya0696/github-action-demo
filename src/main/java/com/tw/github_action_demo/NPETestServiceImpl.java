2cc48bbcc23ff05291036e575d96ed5948d9c190
arghyamitra <arghya.mitra@thoughtworks.com>
2026-05-25 18:56:53 +0530

    externalise build and model and one test case added

--- src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
+++ src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
@@ -1,18 +1,17 @@
 package com.tw.github_action_demo;
 
 import java.util.logging.Logger;
-import org.springframework.stereotype.Service;
 
 
-@Service
 public class NPETestServiceImpl {
 
     private static final Logger LOGGER = Logger.getLogger(NPETestServiceImpl.class.getName());
 
     public void testNPE() {
         final Integer p = null;
-        if (LOGGER.isLoggable(java.util.logging.Level.INFO)) {
-            LOGGER.info(String.valueOf(java.util.Optional.ofNullable(p).map(v -> v.compareTo(10)).orElse(null)));
-        }
+
+        int compareTo = p.compareTo(10);
+
+        System.out.println(compareTo);
     }
 }
