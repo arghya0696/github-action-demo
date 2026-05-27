080dbe12ea63a909f8eb0617559c7a43e4d07efd
arghyamitra <arghya.mitra@thoughtworks.com>
2026-05-27 16:04:58 +0530

    npe test 2

--- src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
+++ src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
@@ -1,12 +1,10 @@
 package com.tw.github_action_demo;
 
-import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-@Service
 public class NPETestServiceImpl {
 
     private static final Logger LOGGER = Logger.getLogger(NPETestServiceImpl.class.getName());
