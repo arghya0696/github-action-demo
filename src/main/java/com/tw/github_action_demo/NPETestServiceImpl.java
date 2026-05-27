28885199351cb739fe0582e6ff9c2175631f52d3
arghyamitra <arghya.mitra@thoughtworks.com>
2026-05-27 18:35:36 +0530

    npe test 2

--- src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
+++ src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
@@ -1,10 +1,9 @@
 package com.tw.github_action_demo;
 
+import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.springframework.stereotype.Service;
 
 
 @Service
@@ -15,23 +14,8 @@ public class NPETestServiceImpl {
     public void testNPE() {
         final Integer p = null;
 
-//            final Integer q =  null;
-//            LOGGER.log(Level.INFO, "Is q equals to 10? ", q.equals(10));
-
-        final int compareTo = Optional.ofNullable(p)
-                .orElse(0)
-                .compareTo(10);
+        final int compareTo = p.compareTo(10);
 
         LOGGER.log(Level.INFO, String.valueOf(compareTo));
-    }
-
-    public void printLogs(){
-        try{
-            Integer i = null;
-            LOGGER.log(Level.INFO, "Is q equals to 10? ", i.equals(10));
-        } catch (Exception e){
-            e.printStackTrace();
-            LOGGER.log(Level.INFO, "Error while performing action : " + e.getMessage());
-        }
     }
 }
