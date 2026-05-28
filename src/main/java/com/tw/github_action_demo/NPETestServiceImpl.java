942da77afadfd2020937e6130a85235a1a8bca37
Sreelakshmi <sreelakshmi.adiga@thoughtworks.com>
2026-05-28 13:59:53 +0530

    feat: scheduled pipe

--- src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
+++ src/main/java/com/tw/github_action_demo/NPETestServiceImpl.java
@@ -12,10 +12,23 @@ public class NPETestServiceImpl {
     public void testNPE() {
         final Integer p = null;
 
+//            final Integer q =  null;
+//            LOGGER.log(Level.INFO, "Is q equals to 10? ", q.equals(10));
+
         final int compareTo = Optional.ofNullable(p)
                 .orElse(0)
                 .compareTo(10);
 
         LOGGER.log(Level.INFO, String.valueOf(compareTo));
+    }
+
+    public void printLogs(){
+        try{
+            Integer i = null;
+            LOGGER.log(Level.INFO, "Is q equals to 10? ", i.equals(10));
+        } catch (Exception e){
+            e.printStackTrace();
+            LOGGER.log(Level.INFO, "Error while performing action : " + e.getMessage());
+        }
     }
 }
